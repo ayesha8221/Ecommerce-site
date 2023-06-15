1e0d477a776b80527a2ca039071fbb1594bb79e6
ayesha8221 <ayesha.glnt012@gmail.com>
2023-06-15 16:30:59 +0200

    admin page

--- script/products.js
+++ script/products.js
@@ -1,4 +1,6 @@
-const products = [
+let products = JSON.parse(localStorage.getItem("products"))
+if (!products) {
+  products = [
     {
       id: 1,
       image: "https://i.postimg.cc/WbQbGKNs/b1418d4e-7bce-4a6d-930e-6a0f8ae0b56d.webp",
@@ -43,111 +45,122 @@ const products = [
       id: 6,
       image: "https://i.postimg.cc/Nf622wTv/a224844e-2a77-4e03-9bd9-843d0b873b7f.jpg",
       name: "Salicylic Acid Cleanser",
-      desc:"",
+      desc: "",
       price: "R125",
       quantity: 5,
     }
-    
-  ];
-  
-  // display products
-  function displayProducts() {
-    const ourProducts = document.getElementById("products");
-    products.forEach((product) => {
-      const productElement = document.createElement("div");
-      productElement.innerHTML = `
-        <img src="${product.image}" alt="${product.name}" id="product-imgs">
-        <h3>${product.name}</h3>
-        <p>${product.price}</p>
-        <button onclick="addToCart(${product.id})" class="addbtn">Add to cart</button>`;
-      ourProducts.appendChild(productElement);
-    });
-  }
 
-  let cart = JSON.parse(localStorage.getItem("Products")) || [];
+  ]
+  localStorage.setItem("products", JSON.stringify(products))
+};
 
-  // Add to cart
-  function addToCart(productId) {
-    const product = products.find((product) => product.id === productId);
-    if (product && product.quantity > 0) {
-      cart.push(product);
-      product.quantity--;
-      updateCart();
-    }
-  }
+function displayProduts() {
+  const ourProducts = document.getElementById("products");
+  products.forEach((product) => {
+    const productElement = document.createElement("div")
+    productElement.innerHTML = `
+    <img src="${product.image}" alt="${product.name}" id="product-imgs">
+           <h3>${product.name}</h3>
+            <p>${product.price}</p>
+             <button onclick="addToCart(${product.id})" class="addbtn">Add to cart</button>
+    `;
+    ourProducts.appendChild(productElement)
+  })
+}
 
-  // remove from cart
-  function removeFromCart(index) {
-    let removedProduct = cart.splice(index, 1)[0];
-    removedProduct.quantity++;
+let cart = JSON.parse(localStorage.getItem("Products")) || [];
+
+// Add to cart
+function addToCart(productId) {
+  const product = products.find((product) => product.id === productId);
+  if (product && product.quantity > 0) {
+    cart.push(product);
+    product.quantity--;
     updateCart();
   }
+}
 
-  // Update cart
-  function updateCart() {
-    const cartContainer = document.getElementById("cart-body");
-    localStorage.setItem("Products", JSON.stringify(cart));
-    cartContainer.innerHTML = "";
-    cart.forEach((product, index) => {
-      const cartItem = document.createElement("div");
-      cartItem.innerHTML = `
+// remove from cart
+function removeFromCart(index) {
+  let removedProduct = cart.splice(index, 1)[0];
+  removedProduct.quantity++;
+  updateCart();
+}
+
+// Update cart
+function updateCart() {
+  const cartContainer = document.getElementById("cart-body");
+  localStorage.setItem("Products", JSON.stringify(cart));
+  cartContainer.innerHTML = "";
+  cart.forEach((product, index) => {
+    const cartItem = document.createElement("div");
+    cartItem.innerHTML = `
         <span>${product.name}</span>
         <span>${product.price}</span>
         <p>Total R ${product.price}</p>
         <button onclick="removeFromCart(${index})" class="rembutton">❌</button>
       `;
-      cartContainer.appendChild(cartItem);
-    });
-    calculateTotal();
-  }
-
-  // Function to calculate
-  function calculateTotal() {
-    let totalElement = document.getElementById("total");
-    let total = cart.reduce((acc, product) => {
-      return acc + parseInt(product.price.replace("R", ""));
-    }, 0);
-    totalElement.textContent = `R${total.toFixed(2)}`;
-  }
-  displayProducts();
-  updateCart();
-
-
-  // Admin page
-  let admin = JSON.parse(localStorage.getItem("adminP"));
-if (!admin) {
-  admin = [];
+    cartContainer.appendChild(cartItem);
+  });
+  calculateTotal();
 }
 
-function addToAdmin () {
+// Function to calculate
+function calculateTotal() {
+  let totalElement = document.getElementById("total");
+  let total = cart.reduce((acc, product) => {
+    return acc + parseInt(product.price.replace("R", ""));
+  }, 0);
+  totalElement.textContent = `R${total}`;
+}
+
+// Admin page
+let admin = JSON.parse(localStorage.getItem("products"));
+if (!admin) {
+  admin = []
+  }
+
+function addToAdmin() {
   let Value = {
     id: document.getElementById("id").value,
     name: document.getElementById("name").value,
     price: document.getElementById("price").value,
     image: document.getElementById("image").value,
-}
+  }
 
   admin.push(Value);
-  localStorage.setItem("adminP", JSON.stringify(admin)); 
-  
+  localStorage.setItem("products", JSON.stringify(products));
 
-  let table = document.querySelector(".display");
-  table.innerHTML = "";
-  admin.forEach((data) => {
-    table.innerHTML += 
-    ` 
+    let table = document.querySelector(".display");
+    table.innerHTML = "";
+    admin.forEach((product, index) => {
+      table.innerHTML +=
+        ` 
     <tbody>
     <tr>
-      <td>${data.id}</td>
-      <td>${data.image}</td>
-      <td>${data.name}</td>
-      <td>${data.price}</td>
+      <td>${product.id}</td>
+      <td>${product.image}</td>
+      <td>${product.name}</td>
+      <td>${product.price}</td>
+      <td><button onclick="removeFromCart(${index})" class="rembutton">❌</button></td>
     </tr>
     <tr>
     </tbody>
     `;
-  
-});
-display();
-
+    });
 }
+
+function loadItems() {
+  let loadProducts = localStorage.getItem("Products");
+  if (loadProducts) {
+    cart = JSON.parse(loadProducts);
+    updateCart();
+  }
+}
+
+window.addEventListener("load", function () {
+  loadItems();
+});
+
+displayProduts();
+updateCart();
